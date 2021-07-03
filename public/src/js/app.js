0873b5364e818b862bd1e92f60c559434ce40cd6
daiju.tsutsumi@forgevision.com <daiju.tsutsumi@forgevision.com>
2021-07-03 19:13:45 +0900

    fecth and promise

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -6,6 +6,9 @@ if("serviceWorker" in navigator) {
     .then(function(){
       console.log("aaaaaaaaaaaaa")
     })
+    .catch((err) => {
+      console.log(err);
+    })
 }
 
 window.addEventListener('beforeinstallprompt', function(event) {
@@ -14,4 +17,47 @@ window.addEventListener('beforeinstallprompt', function(event) {
   deferredPrompt = event;
 
   return false;
+})
+
+var promise = new Promise(function(resolve, reject){
+  setTimeout(function(){
+    resolve("test")
+  }, 3000)
+})
+
+fetch('https://httpbin.org/ip')
+  .then(response => {
+    console.log(response);
+    return response.json();
+  })
+  .then(res => {
+    console.log(res);
+  })
+  .catch(err => {
+    console.error(err);
+  })
+fetch('https://httpbin.org/ip/post', {
+  method: "POST",
+  headers: {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json',
+
+  },
+  body: { message: 'Does this work?'}
+})
+  .then(response => {
+    console.log(response);
+    return response.json();
+  })
+  .then(res => {
+    console.log(res);
+  })
+  .catch(err => {
+    console.error(err);
+  })
+
+  
+
+promise.then((text) => {
+  console.log(text)
 })
